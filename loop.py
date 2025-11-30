8f9c28d62cbb4f56815c024ddf01d9b6df44e634
dhanabal-dev <dhanabalpaskaran@gmail.com>
2025-11-30 19:03:03 +0530

    loop

--- loop.py
+++ loop.py
@@ -1,3 +1,14 @@
 
+# for i in range(1,10):
+#     print(i)
+name=[10,20,30,40,50]
+for i in name:
+    if (i==90):
+        break
+else:
+   print("else is exicuted")
 for i in range(1,10):
-    print(i)
+    if(i==5):
+        continue
+else:
+    print("wrong")
