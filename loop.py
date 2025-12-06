b2e67f5ed3950cc2313029b203d1b24e5a384b5d
dhanabal-dev <dhanabalpaskaran@gmail.com>
2025-12-06 20:46:13 +0530

    loop.py

--- loop.py
+++ loop.py
@@ -12,3 +12,26 @@ for i in range(1,10):
         continue
 else:
     print("wrong")
+for i in range(1,11):
+    print(i,"x2 =",i*2)
+#NEW
+a=8
+b=15
+for i in range(a+1,b):
+    print(i)
+#even no
+for i in range(1,11):
+    if(i%2==0):
+       print(i)
+#count
+c=0
+co=0
+for i in range(1,11):
+    if(i%2==0):
+        c=c+1
+    else:
+        co=co+1
+print(c)
+print(co)
+
+
