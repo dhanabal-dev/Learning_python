5a3af3fa4e3258f2d1ecec0699251685229e4eab
dhanabal-dev <dhanabalpaskaran@gmail.com>
2025-12-06 19:16:32 +0530

    condition_operater.py

--- condition_operater.py
+++ condition_operater.py
@@ -5,3 +5,23 @@ elif(a<11 and a<12):
    print("a is smalest no")
 else:
    print("false")
+#2
+a=int(input())
+if(a%3==0 and a%5==0):
+   print("3 and 5 divided")
+else:
+   print("3 and 5 is not divided")
+#MINI CALCULATER
+a=int(input())
+b=int(input())
+cal=input("add/sub/mult/div")
+if(cal=="add"):
+   print(a+b)
+elif(cal=="sub"):
+   print(a-b)
+elif(cal=="mult"):
+   print(a*b)
+elif(cal=="div"):
+   print(a/b)
+else:
+   print("hi")
