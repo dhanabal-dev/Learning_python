4a9e6b5b9fb20c5f475abaaf218ff3fd47ce7b65
dhanabal-dev <dhanabalpaskaran@gmail.com>
2026-01-18 15:35:43 +0530

    while loop

--- loops/while_loop/while_loop.py
+++ loops/while_loop/while_loop.py
@@ -1,8 +1,29 @@
-"""i=10
+i=10
 while(i==10):
     print(i)
-    i=20"""
+    i=20
 i=1
 while(1<5):
     print(i)
     i=i+1
+print("f")
+i=10
+while(i>0):
+     print(i,end=",")
+     i=i-1
+f=1
+while(f<200):
+     print(f,end=",")
+     f=f+1
+
+g=0
+while(g<5):
+     print(g)
+     g=g+1
+#factorial
+a=3
+fa=1
+while(a>0):#condition true means return go to next line
+     fa=fa*a#1 x3=3
+     a=a-1#(3-1=2)
+print(fa)
